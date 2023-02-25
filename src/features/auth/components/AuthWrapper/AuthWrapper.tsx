afbc9857c0c1d067d25c18b2d8907240fd4b3256
Andrei Petrov <68751397+ndt080@users.noreply.github.com>
2023-02-25 16:18:12 +0300

    feat: install clsx

--- src/features/auth/components/AuthWrapper/AuthWrapper.tsx
+++ src/features/auth/components/AuthWrapper/AuthWrapper.tsx
@@ -2,16 +2,15 @@ import './AuthWrapper.scss';
 
 import { Routes } from '@core/values';
 import { useLoader } from '@features/loader';
+import clsx from 'clsx';
 import { FC, PropsWithChildren } from 'react';
 
 export const AuthWrapper: FC<PropsWithChildren> = function ({ children }) {
   const { isLoading } = useLoader();
 
-  const getLoadingClass = () => (isLoading ? 'border-spinner' : '');
-
   return (
     <main className="auth-wrapper">
-      <div className={`auth-wrapper__container ${getLoadingClass()}`}>
+      <div className={clsx('auth-wrapper__container', isLoading && 'border-spinner')}>
         <div className="auth-wrapper__header">
           <a href={Routes.Home} className="auth-wrapper__logo title-bold-4"></a>
         </div>
